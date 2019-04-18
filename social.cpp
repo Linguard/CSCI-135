ffb3b4b07e8adbb00198955f452d48f841c005f7
Saqibul Islam Chowdhury <37983369+Linguard@users.noreply.github.com>
2019-04-18 13:51:29 -0400

    Create social.cpp

---
+++ social.cpp
@@ -0,0 +1,15 @@
+/*
+Name: Saqibul Islam Chowdhury
+CSCI 136
+Lab Instructor: Anoop Aroor
+In this program, we implement the class Profile that can store the info about a user of the network.
+*/
+
+#include <iostream>
+
+using namespace.std;
+
+int main()
+{
+  
+}
